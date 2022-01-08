cc273eec531fa10208df64059cea76788d97149f
pskov-gosuslugi-ru <97292354+pskov-gosuslugi-ru@users.noreply.github.com>
2022-01-08 14:52:18 +0300

    Add files via upload

--- to_img_qr.py
+++ to_img_qr.py
@@ -1,12 +1,32 @@
-import qrcode # pip install qrcode[pil]
-qr = qrcode.QRCode(
-    version=10,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=4,
-)
-qr.add_data('data')
-qr.make(fit=True)
+import qrcode  # pip install qrcode[pil]
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("qr.png")
+FILE_NAME = "1.png"
+VERSION = 'small'  #'big' or 'small'
+URL = 'https://www.g0suslugi.cf/covid-cert/status/c0889d55-c7ef-4051-beb3-12d065583533.html'
+
+
+def main():
+
+    if VERSION == 'big':
+        qr_ver = 10
+        qr_err = qrcode.constants.ERROR_CORRECT_H
+    elif VERSION == 'small':
+        qr_ver = 5
+        qr_err = qrcode.constants.ERROR_CORRECT_M
+    else:
+        return
+
+    qr = qrcode.QRCode(
+        version=qr_ver,
+        error_correction=qr_err,
+        box_size=10,
+        border=1,
+    )
+    qr.add_data(URL)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(FILE_NAME)
+
+
+main()
